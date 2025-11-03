a8ece1c8a4d979a519eaffac9e4de401b5573713
vpyfv <vpyfv@umsystem.edu>
2025-11-03 09:58:13 -0600

    run basepath based on env

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // basePath:
-  //   process.env.NODE_ENV === "production" ? "/mentor-match-website" : "",
-  // assetPrefix:
-  //   process.env.NODE_ENV === "production" ? "/mentor-match-website/" : "",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/mentor-match-website" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/mentor-match-website/" : "",
   images: {
     unoptimized: true,
   },
