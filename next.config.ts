418a22a382a4a9c98530515eb2eedb432501e75b
vpyfv <vpyfv@umsystem.edu>
2025-11-03 09:51:27 -0600

    static export config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  // basePath:
+  //   process.env.NODE_ENV === "production" ? "/mentor-match-website" : "",
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production" ? "/mentor-match-website/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
