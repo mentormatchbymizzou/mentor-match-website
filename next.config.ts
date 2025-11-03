fa4f134141665613e066b0d3073c8f827a583ab5
vpyfv <vpyfv@umsystem.edu>
2025-11-03 10:10:36 -0600

    static image fix

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath:
-    process.env.NODE_ENV === "production"
-      ? "https://mentormatchbymizzou.github.io/mentor-match-website"
-      : "",
+    process.env.NODE_ENV === "production" ? "/mentor-match-website" : "",
   assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://mentormatchbymizzou.github.io/mentor-match-website/"
-      : "",
+    process.env.NODE_ENV === "production" ? "/mentor-match-website/" : "",
   images: {
     unoptimized: true,
   },
